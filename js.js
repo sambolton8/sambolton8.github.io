4eccd201239ed3ce9e8d78085869016a48f55df5
sambolton8 <40279558@live.napier.ac.uk>
2021-07-14 13:29:15 +0100

    made external js file and linked it

--- js.js
+++ js.js
@@ -1,8 +1,26 @@
-var healthvar = 4;
+var healthvar = 3;
 var goldvar=0;
 				
-function healthStat()
-{
+function healthStat() {
+	if (healthvar <= 4) {
+		document.getElementById("health").style.color = "red";
+			}
+			else {
+				document.getElementById("health").style.color = "green";
+				}
+					return healthvar;		
+			}
+
+				
+function goldStat() {
+	return goldvar;
+			}
+		
+
+function searchWreckage() {
+	alert('You found an apple and 2 gold');
+	(healthvar + 3);
+	(goldvar + 2);
 	return healthvar;
-}
-document.getElementById("health").innerHTML ="Health: " + healthStat();
+	return goldvar;
+}	
